cb736ba089a44e7d8540e4840ab85247baaa4d22
Shreya Swaroop <shreyaswrp@gmail.com>
2020-09-21 17:02:55 +0530

    refactor: move connection of database task to config file

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,20 @@
-module.exports = {
-   url: 'mongodb://localhost:27017/easy-notes'
-}
+/**
+ * Configuring the database
+ */
+require('dotenv/config');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+/**
+ * Connecting to the database
+ */
+const DB_CONFIG = mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+module.exports = mongoose.DB_CONFIG;
--- server.js
+++ server.js
@@ -30,31 +30,16 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-/**
- * Configuring the database
- */
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-/**
- * Connecting to the database
- */
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
-
 require('./routes/greeting.route.js')(app);
+
+require('./config/database.config.js');
+
+require('dotenv/config');
 
 /**
  * listen for requests
  */
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+const PORT = process.env.port || 3000;
+app.listen( PORT, () => {
+    console.log("Server is listening on port"+" " +PORT);
 });
