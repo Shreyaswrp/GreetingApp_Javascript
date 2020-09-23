38694e9a1b006e91884910b060d995a2d0de5046
Shreya Swaroop <shreyaswrp@gmail.com>
2020-09-23 08:06:09 +0530

    refactor: names of routes

--- routes/greeting.route.js
+++ routes/greeting.route.js
@@ -10,23 +10,23 @@ module.exports = (app) => {
         res.json({"message": "Welcome to Greeting application."});
     });
     
+    // Create a new Greeting
+    app.post('/greetingsCreation', greeting.createGreeting);
+
+    // Retrieve all Greetings
+    app.get('/findgreetings', greeting.findAllGreetings);
+
+    // Retrieve a single Greeting with greetingId
+    app.get('/findgreetingById/:greetingId', greeting.findOneGreeting);
+
     /**
      * Define a simple route to display Message by the attributes provided
      */
     app.get("/greetingName/:firstName?/:lastName?", greeting.findGreetingByName);
 
-    // Create a new Greeting
-    app.post('/greetings', greeting.createGreeting);
-
-    // Retrieve all Greetings
-    app.get('/greetings', greeting.findAllGreetings);
-
-    // Retrieve a single Greeting with greetingId
-    app.get('/greetings/:greetingId', greeting.findOneGreeting);
-
     // Update a Greeting with greetingId
-    app.put('/greetings/:greetingId', greeting.updateGreeting);
+    app.put('/greetingsEditing/:greetingId', greeting.updateGreeting);
 
     // Delete a Greeting with greetingId
-    app.delete('/greetings/:greetingId', greeting.deleteGreeting);
+    app.delete('/greetingsDeletion/:greetingId', greeting.deleteGreeting);
 }
